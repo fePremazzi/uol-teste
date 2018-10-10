6f3eaa50f0dcca3dcfa9824394783b758e4e5361
Fellipe Premazzi Rego <felliperego@hotmail.com>
2018-10-10 00:24:13 -0300

    temperatura

---
+++ src/main/java/com/uol/entities/Temperatura.java
@@ -0,0 +1,53 @@
+package com.uol.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "temperatura")
+public class Temperatura {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private Long idTemp;
+	
+	private float tempMin;
+	private float tempMax;
+	
+	@OneToOne(mappedBy = "temp")
+	private Cliente cliente;
+	
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public Long getIdTemp() {
+		return idTemp;
+	}
+	public void setIdTemp(Long idTemp) {
+		this.idTemp = idTemp;
+	}
+	public float getTempMin() {
+		return tempMin;
+	}
+	public void setTempMin(float tempMin) {
+		this.tempMin = tempMin;
+	}
+	public float getTempMax() {
+		return tempMax;
+	}
+	public void setTempMax(float tempMax) {
+		this.tempMax = tempMax;
+	}
+	
+	
+
+}
